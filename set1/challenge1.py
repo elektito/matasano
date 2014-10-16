67485d235a7e681236e825ea0f5a8d7e90e6facb
Mostafa Razavi <mostafa@sepent.com>
2014-10-16 17:34:04 +0330

    Updated challenges 1 and 2 in set 1.
     - Hex encoding and decoding is now taken into account.
     - The two files can now be used properly as modules.

--- set1/challenge1.py
+++ set1/challenge1.py
@@ -1,40 +1,15 @@
-def hex2dec(s):
-    return int(s, 16)
-
-def b64digit(d):
-    if 0 <= d <= 25:
-        return chr(ord('A') + d)
-    elif 26 <= d <= 51:
-        return chr(ord('a') + (d - 26))
-    elif 52 <= d <= 61:
-        return chr(ord('0') + (d - 52))
-    elif d == 62:
-        return '+'
-    elif d == 63:
-        return '/'
-    else:
-        raise Exception('Invalid base64 digit.')
-
-def dec2b64(n):
-    s = ''
-    while n > 64:
-        d = n % 64
-        n = n / 64
-        s += b64digit(d)
-
-    s += b64digit(n)
-
-    return s[::-1]
+import base64
 
 def hex2base64(s):
-    return dec2b64(hex2dec(s))
+    return base64.b64encode(s.decode('hex'))
 
-n = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
-expected = 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
+if __name__ == '__main__':
+    n = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
+    expected = 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
 
-result = hex2base64(n)
+    result = hex2base64(n)
 
-print 'Input is:', n
-print 'Expected:', expected
-print 'Result:', result
-print 'Match!' if result == expected else 'Did not match!'
+    print 'Input is:', n
+    print 'Expected:', expected
+    print 'Result:', result
+    print 'Match!' if result == expected else 'Did not match!'
--- set1/challenge2.py
+++ set1/challenge2.py
@@ -2,15 +2,18 @@ def xor(s1, s2):
     if len(s1) != len(s2):
         raise Exception('Buffer length mismatch.')
 
-    return hex(int(s1, 16) ^ int(s2, 16))[2:-1]
+    return ''.join(chr(ord(i) ^ ord(j))
+                   for i, j
+                   in zip(s1.decode('hex'), s2.decode('hex'))).encode('hex')
 
-expected = '746865206b696420646f6e277420706c6179'
-s1 = '1c0111001f010100061a024b53535009181c'
-s2 = '686974207468652062756c6c277320657965'
-result = xor(s1, s2)
+if __name__ == '__main__':
+    expected = '746865206b696420646f6e277420706c6179'
+    s1 = '1c0111001f010100061a024b53535009181c'
+    s2 = '686974207468652062756c6c277320657965'
+    result = xor(s1, s2)
 
-print 's1:', s1
-print 's2:', s2
-print 'expected:', expected
-print 'result:', result
-print 'Match!' if result == expected else 'Did not match!'
+    print 's1:', s1
+    print 's2:', s2
+    print 'expected:', expected
+    print 'result:', result
+    print 'Match!' if result == expected else 'Did not match!'
